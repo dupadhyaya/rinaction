3d21506244c907e1a84602a25f33d0b89eb2b904
dupadhyaya <dup1966@gmail.com>
2016-06-13 13:13:03 +0530

    13Jun

---
+++ chap2.R
@@ -0,0 +1,11 @@
+#chapter 2 : Creating a Data set
+# exploring data structures, using data entry, importing data, annotating dataset
+# data types - numeric, character, logical, complex, raw
+# Vectors
+a = c(1,2,5,6,-2,4)
+b = c("one", "two", 'three')
+c <- c(TRUE, FALSE, T,F,TRUE,FALSE)
+a ; b; c
+a[3] ; a[c(1,3,5)] ; a[c(2:6)]
+
+# Matrices

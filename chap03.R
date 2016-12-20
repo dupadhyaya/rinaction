cd6f1b22d740a9d7c02b3e3377e8f063826cda38
dupadhyaya <dup1966@gmail.com>
2016-12-20 09:13:54 +0530

    d

--- chap03.R
+++ chap03.R
@@ -1,1 +1,22 @@
 #Chapter - 3 : Getting Started with Graphs ------------------------------------------
+
+#3.1 Working with Graphs
+# Attach mtcars dataset, plot wt vs mpg; draw abline and give title to graph; detach mtcar
+
+attach(mtcars)
+plot(wt,mpg)
+abline(lm(mpg~wt))
+title("Regression of MPG on Weight")
+detach(mtcars)
+
+plot.new()
+attach(mtcars)
+layout(matrix(c(1,1,2,3),2,3,byrow=T))
+hist(wt)
+hist(mpg)
+hist(disp)
+detach(mtcars)
+
+# Listing 3.4 ----------------
+boxplot(mtcars$wt)
+

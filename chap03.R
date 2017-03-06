81516101cced73184363c8d31ed58fa61565c0b4
Dhiraj Upadhyaya <dup1966@gmail.com>
2017-03-06 18:49:47 +0530

    1

--- chap03.R
+++ chap03.R
@@ -57,7 +57,7 @@ par(lty=2)
 par(pch=17)
 
 # Another way with plot command
-plot(dose, drugA, type='b', lty=3, pch=18)
+plot(dose, drugA, type='b', lty=3, pch=18,cex=1.5)
 
 # 3.3.1  Symbols & Lines -----------------
 # pch - plotting sysbol
@@ -135,7 +135,9 @@ plot(dose, drugA, type="b",
 
 title(main="main title", sub="sub-title",
       xlab="x-axis label", ylab="y-axis label")
-plot(c(1,1), ann = F)
+plot(1, ann = F)
+plot(1, ann = T)
+
 title(main="My Title", col.main="red",
       sub="My Sub-title", col.sub="blue",
       xlab="My X label", ylab="My Y label",
@@ -215,25 +217,24 @@ legend(location, title, legend, ....)
 # bty - box type ; bg- background color, cex - size, text.col - text color
 # horiz=T  - legend horizontal 
 # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+par(opar)
+dose <- c(20, 30, 40, 45, 60)
+drugA <- c(16, 20, 27, 40, 60)
+drugB <- c(15, 18, 25, 31, 40)
+opar <- par(no.readonly=TRUE)
+par(lwd=2, cex=1.5, font.lab=2)
+plot(dose, drugA, type="b",
+     pch=15, lty=1, col="red", ylim=c(0, 60),
+     main="Drug A vs. Drug B",
+     xlab="Drug Dosage", ylab="Drug Response")
+lines(dose, drugB, type="b",
+      pch=17, lty=2, col="blue")
+abline(h=c(30), lwd=1.5, lty=2, col="gray")
+library(Hmisc)
+minor.tick(nx=3, ny=3, tick.ratio=0.5)
+legend("topleft", inset=.05, title="Drug Type", c("A","B"),
+       lty=c(1, 2), pch=c(15, 17), col=c("red", "blue"))
+par(opar)
 
 
 attach(mtcars)

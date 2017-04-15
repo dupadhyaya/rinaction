01c5dd0c440d0ee4e016dfbd0aab57157b908e72
Dhiraj Upadhyaya <dup1966@gmail.com>
2017-04-15 17:36:49 +0530

    a

--- practise2.R
+++ practise2.R
@@ -42,3 +42,57 @@ head(dfm)
 
 cast(dfm, am + cyl + variable ~ ., dstats)
 cast(dfm, am + cyl + variable ~.)
+
+
+help(warpbreaks)
+head(warpbreaks)
+with(warpbreaks, table(wool, tension))
+table(state.division, state.region)
+
+course = data.frame(course= c("bsc","msc","phd", "bsc","msc","phd"),
+                    gender=c('male','male','male','female','female','female' ),
+                    students=c(10,15,7,12,20,10))
+course
+ftable(course$course, course$gender, course$students)
+xtabs(~course + gender + students, data=course)
+
+student <- matrix(c(10,12,15,20,7,10),ncol=2,byrow=TRUE)
+colnames(student) <- c('male','female')
+rownames(student) <- c("bsc","msc","phd")
+student <- as.table(student)
+student
+barplot(student)
+plot(student)
+margin.table(student)
+margin.table(student,1)
+margin.table(student,2)
+prop.table(student)
+prop.table(student,1)
+prop.table(student,2)
+
+addmargins(student)
+addmargins(student,1)
+addmargins(student,2)
+
+
+
+ID <- seq(1:177)
+Age <- sample(c("0-15", "16-29", "30-44", "45-64", "65+"), 177, replace = TRUE)
+Sex <- sample(c("Male", "Female"), 177, replace = TRUE)
+Country <- sample(c("England", "Wales", "Scotland", "N. Ireland"), 177, replace = TRUE)
+Health <- sample(c("Poor", "Average", "Good"), 177, replace = TRUE)
+Survey <- data.frame(Age, Sex, Country, Health)
+head(Survey)     
+source("http://pcwww.liv.ac.uk/~william/R/crosstab.r")
+crosstab(Survey, row.vars = "Age", col.vars = "Sex", type = "f")   
+view(crosstab)
+#http://rstudio-pubs-static.s3.amazonaws.com/6975_c4943349b6174f448104a5513fed59a9.html
+# Row percentages
+crosstab(Survey, row.vars = "Age", col.vars = "Sex", type = "r")
+# Column percentages
+crosstab(Survey, row.vars = "Age", col.vars = "Sex", type = "c")
+# Joint percentages (sums to 100 within final two table dimensions)
+crosstab(Survey, row.vars = c("Age", "Sex"), col.vars = "Health", type = "j")
+# Total percentages (sums to 100 across entire table)
+crosstab(Survey, row.vars = c("Age", "Sex"), col.vars = "Health", type = "t")
+         
